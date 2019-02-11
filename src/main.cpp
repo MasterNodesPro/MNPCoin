db8b7704ad3623133364ca02c4094071ab36865e
j00v <jules00verne@gmail.com>
2019-02-10 19:40:40 -0500

    Phore [e3cebe0]
    Fix bug not allowing some types of forks to occur.

--- src/main.cpp
+++ src/main.cpp
@@ -4607,7 +4607,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                     if (it == mapStakeSpent.end()) {
                         return false;
                     }
-                    if (it->second <= pindexPrev->nHeight) {
+                    if (it->second < pindexPrev->nHeight) {
                         return false;
                     }
                 }
