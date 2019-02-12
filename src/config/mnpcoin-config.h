ee24411bda0850b578d64c2956e0b1da2841b24d
j00v <jules00verne@gmail.com>
2019-02-12 03:55:59 +0100

    Updated version to v1.2.3

--- src/config/mnpcoin-config.h
+++ src/config/mnpcoin-config.h
@@ -21,7 +21,7 @@
 #define CLIENT_VERSION_MINOR 2
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 2
+#define CLIENT_VERSION_REVISION 3
 
 /* Version is release */
 #define COPYRIGHT_YEAR 2019
@@ -274,7 +274,7 @@
 #define PACKAGE_NAME "MNPCoin Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "MNPCoin Core 1.0.0"
+#define PACKAGE_STRING "MNPCoin Core 1.2.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "mnpcoin"
@@ -283,7 +283,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.0"
+#define PACKAGE_VERSION "1.2.3"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
