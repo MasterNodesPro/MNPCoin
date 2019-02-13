2689c068a0fe5a735b08a50aed77b594adebcb35
MNP_Jason <jason@masternodes.pro>
2019-02-12 22:30:50 -0600

    Change out Min protocal update to 70010

--- src/version.h
+++ src/version.h
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70020;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70010;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70030;
 
 //! nTime field added to CAddress, starting with this version;
