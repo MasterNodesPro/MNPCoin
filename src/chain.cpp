689ea70c8657daea1682aef9bade30565fbbaf86
mnp_jason <jason@masternodes.pro>
2019-02-25 14:38:17 -0600

    Remove Lingering BIP130 Code

--- src/chain.cpp
+++ src/chain.cpp
@@ -54,8 +54,8 @@ CBlockLocator CChain::GetLocator(const CBlockIndex* pindex) const
 
 const CBlockIndex* CChain::FindFork(const CBlockIndex* pindex) const
 {
-    if (!pindex)
-        return nullptr;
+//    if (!pindex)
+//        return nullptr;
 
     if (pindex->nHeight > Height())
         pindex = pindex->GetAncestor(Height());
