491a9eb85c5604e0837a664bffaf90d21cb6a1a5
mnp_jason <jason@masternodes.pro>
2019-02-25 16:49:08 -0600

    Protocol Enforcement Spork setup

--- src/main.cpp
+++ src/main.cpp
@@ -6833,11 +6833,12 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
-    if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
+//    if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
+//            return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
+
+    if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
             return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
 
-//    if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
-//            return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
     return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
 }
 
--- src/version.h
+++ src/version.h
@@ -20,8 +20,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70010;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70030;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70030;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70040;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
